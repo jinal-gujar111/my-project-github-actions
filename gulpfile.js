53d9e0a5eb13932802c7923f500ac7fcf983ecdd
jinal-gujar111 <124512722+jinal-gujar111@users.noreply.github.com>
2024-02-22 17:59:48 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ const gulp = require('gulp');
 
 // Define a simple Gulp task
 gulp.task('demoTask', function() {
-  console.log('Demo task executed!');
+  return new Promise((resolve) => {
+    console.log('Demo task executed!');
+    resolve(); // Signal async completion
+  });
 });
 
 // Default task - runs when you type 'gulp' in the terminal
